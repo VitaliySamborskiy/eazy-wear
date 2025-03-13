b6bc9731d0adc9f1c55971f6be2f1db51febf46d
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-03-13 21:16:29 +0200

    add routers

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,20 @@ import { createRoot } from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import { Layout, MainPage } from "./pages/pages.tsx";
+import { AppRouter } from './enums/app-router.enum.ts';
+
 const router = createBrowserRouter([
 	{
 		path: '/',
-	}
+		element: <Layout/>,
+		children: [
+			{
+				element: <MainPage />,
+				path: AppRouter.MAINPAGE,
+			},
+		],
+	},
 ])
 
 createRoot(document.getElementById("root")!).render(
---
+++ src/pages/pages.tsx
@@ -0,0 +1,2 @@
+export { Layout } from "./layout/layout.js";
+export { MainPage } from "./main-page/main-page.js";
