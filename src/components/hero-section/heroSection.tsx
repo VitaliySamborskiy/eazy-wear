f0d70e2f94c31eff73e1114ee408e53e4ab0d521
MaxBoPeep <mihalcukmaksim83@gmail.com>
2025-04-03 15:57:51 +0300

    Оновлення Hero Section

--- src/components/hero-section/heroSection.tsx
+++ src/components/hero-section/heroSection.tsx
@@ -1,26 +1,24 @@
-import React from "react";
 import styles from "./heroSection.module.scss";
-import heroImage from  "../../assets/images/ymen.png";
+import heroImage from "../../assets/images/ymen.png";
 
-export const HeroSection: React.FC = () => {
+const HeroSection = () => {
   return (
     <section className={styles.hero}>
-      {/* background img */}
-      <div className={styles.background}>
+      <picture className={styles.background}>
         <img src={heroImage} alt="Hoodie" />
-      </div>
+      </picture>
 
-      {/* Content */}
       <div className={styles.content}>
-        <h1>YOUR STYLE, <br /> YOUR FREEDOM</h1>
+        <h1>YOUR STYLE, YOUR FREEDOM</h1>
         <p>
           Online - men's clothing store <strong>EAZEWEAR</strong>, always happy to welcome new customers!
         </p>
         <button className={styles.button}>Details</button>
       </div>
 
-      {/* Text on the background */}
-      <h1 className={styles.bgText}>EAZYWEAR</h1>
+      <span className={styles.bgText}>EAZYWEAR</span>
     </section>
   );
 };
+
+export { HeroSection };
