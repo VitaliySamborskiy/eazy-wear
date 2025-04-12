47424a9477bf521ac02d662a1b85b71cbcc90c6a
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-04-12 20:07:32 +0300

    added a workpiece for robots with different styles

--- src/components/swiper/types/types.ts
+++ src/components/swiper/types/types.ts
@@ -1,12 +1,17 @@
 import React from "react";
 import { type Category } from "../../category-card/types/types.ts";
 
-export type SwiperProps = {
+type SwiperSection = "category" | "popular" | "reviews";
+
+type SwiperProps = {
 	slidesPerView?: number;
 	spaceBetween?: number;
 	loop?: boolean;
 	swiperBreakpoints?: Record<number, Record<string, number>>;
+	swiperSupportStyles?: SwiperSection;
 	title: string;
 	slideComponents: React.FC<Category>;
 	slideData: Category[];
 };
+
+export type { SwiperProps };
