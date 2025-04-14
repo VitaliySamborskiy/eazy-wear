318fae3a5610b98d206c4da86fe80d7eede78db6
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-04-14 15:58:42 +0300

    redesigned the about us section and made it responsive

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -3,4 +3,5 @@ export { BurgerMenu } from "./burger-menu/burger-menu.tsx";
 export { IconElement } from "./icons/icon-element.tsx";
 export { SwiperSection } from "./swiper/swiper.tsx";
 export { CategoryCard } from "./category-card/category-card.tsx";
+export { AboutUs } from "./about-us/about-us.tsx";
 export { SvgSprite } from "./icons/svg-sprite.tsx";
