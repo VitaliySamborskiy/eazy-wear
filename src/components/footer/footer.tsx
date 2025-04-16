df95e8c03a39ef722799ead62f2d9ce69ce0aee0
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2025-04-16 22:02:04 +0300

    redesigned and added styles and added layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -23,45 +23,60 @@ const Footer: React.FC = () => {
 	];
 
 	return (
-		<footer className={styles.footer}>
-			<div className="footer__container">
-				<div className={styles.content}>
-					<div className={styles.logoBlock}>
-						<Link
-							to={AppRouter.MAINPAGE}
-							className={styles.logo}>
-							EAZYWEAR
-						</Link>
-						<p>{t("footer.slogan")}</p>
-					</div>
+		<footer className="footer__container">
+			<div className={styles.newsSection}>
+				<div className={styles.newsText}>
+					<h2>{t("footer.news.title")}</h2>
+					<p>{t("footer.news.description")}</p>
+				</div>
+				<form
+					className={styles.form}
+					name="numbersSubscribeForm">
+					<input
+						id="numbersSubscribe"
+						name="numbersSubscribe"
+						type="tel"
+						placeholder="+38 (_ _ _) _ _ _"
+					/>
+					<button>{t("footer.news.button")}</button>
+				</form>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.logoBlock}>
+					<Link
+						to={AppRouter.MAINPAGE}
+						className={styles.logo}>
+						EAZYWEAR
+					</Link>
+					<p>{t("footer.slogan")}</p>
+				</div>
 
-					<div className={styles.info}>
-						<h3 className={styles.subTitle}>{t("footer.info")}</h3>
-						<nav aria-label={t("footer.aria.menu")}>
-							{links.map(item => (
-								<Link
-									key={`Link` + item.router}
-									to={item.router}
-									className={styles.link}>
-									{t(item.text)}
-								</Link>
-							))}
-						</nav>
-					</div>
+				<div className={styles.info}>
+					<h3 className={styles.subTitle}>{t("footer.info")}</h3>
+					<nav aria-label={t("footer.aria.menu")}>
+						{links.map(item => (
+							<Link
+								key={`Link` + item.router}
+								to={item.router}
+								className={styles.link}>
+								{t(item.text)}
+							</Link>
+						))}
+					</nav>
+				</div>
 
-					<div className={styles.social}>
-						<h3>{t("footer.social")}</h3>
-						<nav aria-label={t("footer.aria.social")}>
-							{socialMedia.map(social => (
-								<a
-									key={social}
-									href="#"
-									className={styles.socialLink}>
-									<IconElement {...{ name: social, ...baseIconParameters }} />
-								</a>
-							))}
-						</nav>
-					</div>
+				<div className={styles.social}>
+					<h3 className={styles.subTitle}>{t("footer.social")}</h3>
+					<nav aria-label={t("footer.aria.social")}>
+						{socialMedia.map(social => (
+							<a
+								key={social}
+								href="#"
+								className={styles.socialLink}>
+								<IconElement {...{ name: social, ...baseIconParameters }} />
+							</a>
+						))}
+					</nav>
 				</div>
 			</div>
 		</footer>
