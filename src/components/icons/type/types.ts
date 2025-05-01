65eec248745a0d00a441b47dc2851d3741f46644
Vitaliy <vitaliysamborsky2002@gmail.com>
2025-05-01 11:53:58 +0300

    fix: Refactor SVG attributes and update styles for consistency
    Replaced deprecated JSX attributes with camelCase counterparts in SVG components. Adjusted layout styles for `burger-menu` and `header` to improve design responsiveness and visual alignment. Added type definitions for icon names in the TypeScript file.

--- src/components/icons/type/types.ts
+++ src/components/icons/type/types.ts
@@ -1,6 +1,21 @@
+type IconNames =
+	| "telegram"
+	| "facebook"
+	| "instagram"
+	| "heart"
+	| "basket"
+	| "search"
+	| "cross"
+	| "registration"
+	| "productSearch"
+	| "cart"
+	| "popularProducts"
+	| "dropshipping"
+	| "discounts";
+
 export type Icon = {
 	style?: string;
-	name: string;
+	name: IconNames;
 	width?: number;
 	height?: number;
 	color?: string;
